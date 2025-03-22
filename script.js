f94edbdb13a5bee8c21bb12a47561f7fe8e7db0e
boeing-byte <boardmanlewis@gmail.com>
2025-03-22 07:35:28 +0000

    Added hover drawing functionality

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ createGrid(40);
 
 
 function createGrid(dimension){
+    const grid = document.createElement("div") ;
+    grid.className = "grid";
+    container.appendChild(grid);
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement("div")
         row.className = `row ${i}`;
-        container.appendChild(row);
+        grid.appendChild(row);
         for (let i = 0; i < dimension; i++){
             const cell = document.createElement("div");
             cell.className = "cell";
-            cell.addEventListener("mousedown", () => {cell.style.backgroundColor= "black"})
             row.appendChild(cell)
+            cell.addEventListener("mouseenter", () => {cell.style.backgroundColor = "black"});
         }
     }
 }
 
+function resetBgColor (cell) {
+    cell.style.backgroundColor = "white";
+}   
+
+// function changeCellColor (cell) {
+//     console.log();
+//     cell.style.backgroundColor = "black";
+// }
 
 
-    
+// const reset = document.querySelector(".reset")
+
+// reset.addEventListener("click", function(cell){
+//     grid.remove()
+// })
+
+// trying to work out how to reset the grid to all white squares when pressing the reset button
